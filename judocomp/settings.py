7979a7f1fa3ee7c0c4296913ae4a9036d2fd01a5
Vinicius Oliveira <seu_email_do_github@example.com>
2025-12-03 11:40:27 -0300

    Corrigir configuração de arquivos estáticos removendo STATICFILES_STORAGE duplicado

--- judocomp/settings.py
+++ judocomp/settings.py
@@ -170,13 +170,11 @@ USE_THOUSAND_SEPARATOR = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'atletas' / 'static',
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (uploads)
 MEDIA_URL = '/media/'
